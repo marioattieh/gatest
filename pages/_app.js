92b77aa669607fb543dd9006d70bdc520259ed66
marioattieh <mario.attieh.2@gmail.com>
2022-03-21 17:39:40 +0200

    Last Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,15 @@
-import { useEffect } from "react";
 import Script from "next/script";
-import { useRouter } from "next/router";
-import { pageview } from "../pages/lib/gtm";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    router.events.on("routeChangeComplete", pageview);
-    return () => {
-      router.events.off("routeChangeComplete", pageview);
-    };
-  }, [router.events]);
-
   return (
     <>
-      {/* Google Tag Manager - Global base code */}
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', 'G-WZD247MMFQ');
-          `,
-        }}
-      />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-WZD247MMFQ" />
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-WZD247MMFQ');`}
+      </Script>
       <Component {...pageProps} />
     </>
   );
