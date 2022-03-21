f5d5c4484fcce43c0e7f70a9e4fc9e4adcaac70d
marioattieh <mario.attieh.2@gmail.com>
2022-03-21 11:16:38 +0200

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
+
+const tagManagerArgs = {
+  gtmId: "G-226G38XNWT",
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
