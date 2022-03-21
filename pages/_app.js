f7d18574bf4f91fa3d1980e96e506b0e878238d3
marioattieh <mario.attieh.2@gmail.com>
2022-03-21 13:16:55 +0200

    F Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,35 @@
-import "../styles/globals.css";
-import TagManager from "react-gtm-module";
 import { useEffect } from "react";
-
-const tagManagerArgs = {
-  gtmId: "G-WZD247MMFQ",
-};
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { pageview } from "../pages/lib/gtm";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-    dataLayer.push({
-      event: "pageview",
-      page: window.location.pathname,
-    });
-  }, []);
-  return <Component {...pageProps} />;
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'G-WZD247MMFQ');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
